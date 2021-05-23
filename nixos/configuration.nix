7c395076296617b380aaeb0d4cb1fb8cb30643b4
Wyatt Osterling <48140117+wizardwatch@users.noreply.github.com>
2021-05-23 00:19:24 -0400

    Add files via upload

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -5,14 +5,25 @@
 { config, pkgs, ... }:
 
 {
+  # edit the version of discord. I could not get it to work. 
+  #version = "0.0.121";
+  #nixpkgs.overlays = [(self: super: { discord = super.discord-canary.overrideAttrs (_: { src = builtins.fetchTarball https://dl-canary.discordapp.net/apps/linux/0.0.121/discord-canary-0.0.121.tar.gz; });})];
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
+  # sacrifice freedom for ease
   nixpkgs.config = {
     allowUnfree = true;
   };
-  # Use the systemd-boot EFI boot loader.
+  # let me install random packages from githubs. Like the AUR, but even less secure!
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+   };
+  # Use the systemd-boot EFI boot loader. no touchy
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -26,8 +37,13 @@
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
   networking.useDHCP = false;
-  networking.interfaces.enp0s31f6.useDHCP = true;
-  networking.networkmanager.enable = true;
+  networking.interfaces.enp0s31f6.ipv4.addresses = [ {
+    address = "192.168.1.169";
+    prefixLength = 24;
+  } ];
+  networking.defaultGateway = "192.168.1.1";
+  networking.nameservers = [ "192.168.1.146" "1.1.1.1" ];
+  # networking.networkmanager.enable = true;
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -39,9 +55,10 @@
   #   keyMap = "us";
   # };
 
+  # No more x! No more x!
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
+  # services.xserver.enable = true;
+  services.xserver.displayManager.lightdm.enable = false;
 
   # Enable the GNOME 3 Desktop Environment.
   # services.xserver.displayManager.gdm.enable = true;
@@ -53,7 +70,7 @@
   # services.xserver.xkbOptions = "eurosign:e";
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
 
   # Enable sound.
   # sound.enable = true;
@@ -65,32 +82,43 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.wyatt = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    shell = pkgs.nushell;
+    #shell = pkgs.nushell;
   };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
 	environment.systemPackages = with pkgs; [
- 		polkit_gnome	
-		git    
+    ## password entry for gui applications
+    polkit_gnome
+    ## source control; linus style
+		git
+    ## get the web, from your own computer
 		wget
+    ## firefox with a touch of the farside
 		firefox-wayland
     ### UNFREE ###
+    # dam discord and their tos. I want a decent client. 
     discord-canary
     ### Free ###
-    electron
+    ## bloat just got bloated
+    # electron
+    ## the text editor of the past, today
     emacs
+    ## monitor all the things, except gpu usage
 		htop
+    ## python
     python39
+    ## is it wrong to use a pulse audio tool with pipewire
 		pavucontrol
+    ## emacs thing I think. no touchy
     ripgrep
+    ## stupid noncompliant websites
     chromium
-    # for parsec maybe
-    flatpak
     ### emacs deps
     # for emacs pdfs
     poppler
@@ -98,30 +126,131 @@
     texlive.combined.scheme-full
     # for emacs github/magit forge
     gnupg
+    # for decrypting .authinfo
     pinentry-emacs
     pinentry
-    ###
+    ## no longer using nushell. It is too nu
     nushell
+    ## the prefered way to install rust
     rustup
-    jdk11
+    ## god I hate java
+    # jdk11
+    ## those videos aren't going to download themselves!
     youtube-dl
+    ## if only I could draw
     krita
+    ## emacs and other stuff dependency. 
     sqlite
+    ## pipewire equalizer
     pulseeffects-pw
-    pass
-    ## Spell check only teted in emacs
+    ## local password manager. Replaced by 'the cloud'
+    # pass
+    ## Spell check only tested in emacs
     hunspell
     hunspellDicts.en_US-large
     ## desktop notifications
     libnotify
     ## terminal pdf compressor
     ghostscript
+    ## file browser
+    gnome3.nautilus
+    # doesn't work due to a lack of the overall gnome package group
+    # gnome3.gnome-tweak-tool
+    ## krita fix icons
+    breeze-icons
+    ## java extra credit
+    # greenfoot
+    ## remote into ras-pi
+    nomachine-client
+    ## doesn't seem to work on wayland
+    #lxappearance
+    obs-studio
+    ## obs for wlroots
+    obs-wlrobs
+    ## password manager
+    bitwarden
+    bitwarden-cli
+    ## email, like snail mail, but harder to block the spam!
+    mailspring
+    ## personal site
+    nodePackages.gatsby-cli
+    nodePackages.npm
+    nodejs
+    autoconf
+    automake
+    libtool
+    libtool_1_5
+    autogen
+    autobuild
+    coreutils-full
+    pkgconfig
+    nasm
+    libpng
+    dpkg
+    gettext
+    intltool
+    mozjpeg
+    ## boring work just got a little more mundane
+    libreoffice-fresh
+    ## HSCTF
+    hping
+    nmap
+    masscan
+    openvpn
+    curlFull
+    dig
+    mitmproxy
+    p0f
+    hcxdumptool
+    hcxtools
+    bully
+    aircrack-ng
+    smbclient
+    openssl
+    hashcat
+    john
+    file
+    exiftool
+    hexdump
+    vim
+    binutils
+    gdb-multitarget
+    ghidra-bin
+    burpsuite
+    zap
+    ruby
+    perl
+    python2Full
+    thc-hydra
+    gobuster
+    rsync
+    moreutils
+    sqlmap
+    metasploit
 	];
+  networking.wireguard.enable = true;
 	fonts.fonts = with pkgs; [
-		jetbrains-mono
+    ## for everything except :
+    jetbrains-mono
+    ## for emacs, duh
 		emacs-all-the-icons-fonts
+    ## for waybar icons
     font-awesome
+    ## for krita, might not need this for you have it above as a pkg, but hey why not
+    breeze-icons
+    ## non monospaced text sexifier.
+    roboto
 	];
+  fonts.fontconfig.defaultFonts.monospace = [
+    "JetBrains Mono"
+  ];
+  fonts.fontconfig.defaultFonts.sansSerif = [
+    "Roboto-Regular"
+  ];
+  ## fixes some problems problems with pure gtk applications a little bit.
+  fonts.fontconfig.hinting.enable = false;
+  ## made some fonts look really bad
+  #fonts.fontconfig.antialias = false;
 	programs.sway = {
   	enable = true;
  		wrapperFeatures.gtk = true; # so that gtk works properly
@@ -138,6 +267,8 @@
 		  oguri
 		  grim
 		  slurp
+      ## gtk theme
+      dracula-theme
   	];
 	};
 	#
@@ -164,15 +295,29 @@
     	enable = true;
     	extraPortals = with pkgs; [
       	xdg-desktop-portal-wlr
-      	xdg-desktop-portal-gtk
+        # xdg-desktop-portal-gtk
     	];
-    	gtkUsePortal = true;
+      ## fixes gtk themeing so that it uses the .config. set to true in order to use native file pickers
+    	gtkUsePortal = false;
 		};
 	};
 	environment.sessionVariables = {
+    ### probably not needed due to firefox-wayland
    	MOZ_ENABLE_WAYLAND = "1";
-   	XDG_CURRENT_DESKTOP = "sway"; 
+    ### makes emacs use .config instead of the home dir. ~/.config breaks at least sway
+	  XDG_CONFIG_HOME = "/home/wyatt/.config";
+    ### shouldn't be needed but some software is bad
+   	XDG_CURRENT_DESKTOP = "sway";
+    ### fixes some ugly. TODO: more work on the right numbers
+    GDK_SCALE = "1.5";
+    GDK_DPI_SCALE = "1";
 	};
+  
+  #
+  # flatpak
+  #
+  # services.flatpak.enable = true;
+  
   #
   # pinentry
   #
@@ -181,10 +326,18 @@
     enableSSHSupport = true;
     pinentryFlavor = "emacs";
   };
+  ###
+  ### UNFREE
+  ###
+  # for the games!
   programs.steam.enable = true;
   hardware.steam-hardware.enable = true;
-  #programs.gnupg.agent.pinentryFlavor = "emacs";
-    # Some programs need SUID wrappers, can be configured further or are
+  ###
+  ###
+  ###
+  # Let the passwords be stored in something other than plain text. Required for at least mailspring
+  services.gnome.gnome-keyring.enable = true;
+  # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
@@ -211,5 +364,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "20.09"; # Did you read the comment?
-
 }
